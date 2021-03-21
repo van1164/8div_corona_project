07c41ea8ad0db835c533092511240212fa06eb24
van1164 <52437971+van1164@users.noreply.github.com>
2021-03-21 20:02:17 +0900

    sd

--- checklist/checklist/settings.py
+++ checklist/checklist/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'checklist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
         'USER' : 'van133'
     }
 }
@@ -125,14 +125,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-
-    BASE_DIR / "static",
-]
-
-STATIC_ROOT = BASE_DIR.parent / "static_cdn"
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT =  BASE_DIR / "media"
